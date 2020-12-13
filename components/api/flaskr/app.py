78e719a0440ecfe7d152a945b3877547c171182f
Alexandre Masselot <alexandre.masselot@gmail.com>
2020-12-13 22:04:32 +0100

    load dictionary by appending punction and numbers on short

--- components/api/flaskr/app.py
+++ components/api/flaskr/app.py
@@ -9,7 +9,7 @@ from flaskr.passwordChecker.substitute import SubstitutionParams
 app = Flask(__name__)
 
 load_all_dictionaries('flaskr/resources/dictionaries/250',
-                      SubstitutionParams(substitute_characters=2, append_punctuation=False, append_number=False))
+                      SubstitutionParams(substitute_characters=2, append_punctuation=True, append_number=True))
 load_all_dictionaries('flaskr/resources/dictionaries/long',
                       SubstitutionParams(substitute_characters=0, append_punctuation=False, append_number=False))
 
